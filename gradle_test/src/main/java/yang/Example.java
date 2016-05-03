0504abe50514efbe5977727953d1d782c9612908
moohuel@naver.com <moohuel@naver.com>
2016-05-03 22:43:29 +0900

    my example

---
+++ gradle_test/src/main/java/yang/Example.java
@@ -0,0 +1,12 @@
+package yang;
+
+/**
+ * Created by yang on 2016-05-03.
+ */
+public class Example
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello Gradle");
+    }
+}
